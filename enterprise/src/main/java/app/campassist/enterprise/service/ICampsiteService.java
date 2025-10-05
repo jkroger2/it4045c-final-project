a5b222938662ef0784d301646557e96e5502df9c
Jay Kroger <jaykroger2@gmail.com>
2025-10-05 17:22:41 -0400

    Creating initial ICampsiteService Interface

--- enterprise/src/main/java/app/campassist/enterprise/service/ICampsiteService.java
+++ enterprise/src/main/java/app/campassist/enterprise/service/ICampsiteService.java
@@ -1,5 +1,38 @@
 package app.campassist.enterprise.service;
 
-public interface ICampsiteService {
+import java.util.List;
 
+import app.campassist.enterprise.dto.Campsite;
+
+public interface ICampsiteService {
+    /**
+     * Fetch a campsite by its ID.
+     * @param id The unique identifier of the campsite.
+     * @return The campsite object if found, otherwise null.
+     */
+    Campsite fetchCampsiteById(String id);
+
+    /**
+     * Fetch all campsites.
+     * @return A list of all campsite objects.
+     */
+    List<Campsite> fetchAllCampsites();
+
+    /**
+     * Add a new campsite.
+     * @param campsite The campsite object to add.
+     */
+    void addCampsite(Campsite campsite);
+
+    /**
+     * Update an existing campsite.
+     * @param campsite The campsite object with updated information.
+     */
+    void updateCampsite(Campsite campsite);
+
+    /**
+     * Delete a campsite by its ID.
+     * @param id The unique identifier of the campsite.
+     */
+    void deleteCampsite(String id);
 }
