b600741d5ca1913342a8d87eca83e5ecec18a069
Jay Kroger <jaykroger2@gmail.com>
2025-10-05 16:22:27 -0400

    Creating Campsite DTO class

---
+++ enterprise/src/main/java/app/campassist/enterprise/dto/Campsite.java
@@ -0,0 +1,20 @@
+package app.campassist.enterprise.dto;
+
+import java.util.List;
+import lombok.Data;
+
+public @Data
+class Campsite {
+    private String id;
+    private String name;
+    private String description;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private double latitude;
+    private double longitude;
+    private List<String> amenities;
+    private List<String> images;
+    private List<String> tags;
+}
